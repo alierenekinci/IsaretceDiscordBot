789b225a3dccd1f0bb4fc0902a22867a4df0f468
Ali Eren Ekinci <mralierenekinci@gmail.com>
2021-01-17 15:17:37 +0300

    init

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import discord
 
 
-def sorgu(text):
+def getData(text):
 
     link = "https://isaretce.com/?s=" + text
     site = requests.get(link)
+   
     siteIcerigi = BeautifulSoup(site.content, "html5lib")
+
     if "Üzgünüz, ancak aradığınız kelimeyi bulamadık." in str(siteIcerigi):
         return "Üzgünüz, ancak aradığınız kelimeyi bulamadık."
     else:
         title = siteIcerigi.find("h1", attrs={"class","entry-title"}).string
         gifLink = siteIcerigi.find("img", attrs={"class", "alignnone"}).get("src")
-        vs = siteIcerigi.find_all("p")[-1].getText()
+        #description = siteIcerigi.find("div", attrs={"class","column"}).string
+    
+        description = siteIcerigi.find_all("p")[-1].getText()
 
-        return title, gifLink, vs
+        return title, gifLink, description
+
 text = input()
-text.replace()
+print(getData(text))
