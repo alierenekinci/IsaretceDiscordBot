d10e6706e72d39d00a5bc96f8ec21fd9df43091e
Ali Eren Ekinci <mralierenekinci@gmail.com>
2021-01-21 09:29:36 +0300

    Added Headers :)

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import discord
-
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 def getData(text):
 
     link = "https://isaretce.com/?s=" + text
-    site = requests.get(link)
+    site = requests.get(link, headers=headers)
     siteIcerigi = BeautifulSoup(site.content, "html5lib")
 
     if "Üzgünüz, ancak aradığınız kelimeyi bulamadık." in str(siteIcerigi):
